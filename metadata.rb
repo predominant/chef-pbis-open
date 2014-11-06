a7edaca6ff3811c1b37c4d6a68686a00a6baef3a
troyready <troy@troyready.com>
2014-11-05 18:26:14 -0800

    version bump to v1.0.4

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jared.king@biola.edu'
 license          'Apache 2.0'
 description      'Installs and configures PowerBroker Identity Services Open Edition'
 long_description 'Installs and configures PowerBroker Identity Services Open Edition'
-version          '1.0.3'
+version          '1.0.4'
 
 depends          'chef-vault', '~> 1.1.2'
