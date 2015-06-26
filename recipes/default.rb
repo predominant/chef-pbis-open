e6c547be5f882da8bcf05f272033e6c6594a2f7a
Andrew Dutton <andrew.dutton@rakops.com>
2015-06-26 16:23:37 -0400

    Add in support for surpressing reboot by attribute. Clean up some some unused attributes.

--- recipes/default.rb
+++ recipes/default.rb
@@ -43,7 +43,7 @@ begin
     bind_credentials = chef_vault_item(node['pbis-open']['chef_vault'], node['pbis-open']['chef_vault_item'])
   else
     log 'Using Encrypted data bag for AD credentials'
-    secret = Chef::EncryptedDataBagItem.load_secret()
+    secret = Chef::EncryptedDataBagItem.load_secret
     bind_credentials = Chef::EncryptedDataBagItem.load(node['pbis-open']['data_bag'], node['pbis-open']['data_bagitem'], secret)
   end
 
@@ -67,11 +67,23 @@ if File.exist?('/usr/bin/domainjoin-cli') && domain_member == 1
   end
 # Join the computer to the domain if needed
 elsif bind_credentials
+  
+
+  reboot 'now' do
+    action :nothing
+    reason 'Reboot for pbis installation.'
+    delay_mins 1
+    only_if { node['pbis-open']['perform_reboot'] }
+  end
+  
   execute 'join-domain' do
     sensitive true
     command "domainjoin-cli join #{node['pbis-open']['ad_domain'].upcase} #{bind_credentials['username']} '#{bind_credentials['password']}'"
     action :run
+    notifies :reboot_now, 'reboot[now]', :immediately
   end
+
+
 end
 
 # Disable the Ohai passwd plugin to avoid pulling LDAP information
